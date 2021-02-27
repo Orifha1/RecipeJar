623fb5fed3bb55f6be5d5a2e0e1937b223d83d04
Orifha1 <orifhatshikotshi1@gmail.com>
2021-02-27 14:56:20 +0200

    add register logic

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const User = require('../models/user')
+const catchAsync = require('../utils/catchAsync');
 
-//const catchAsync = require('../utils/catchAsync');
-
+//render register form
 router.get('/register', (req,res) =>{
     res.render('users/register');
-})
-router.post('/register', async (req, res) => {
-    res.send(req.body);
-})
+});
+
+//register a new user
+router.post('/register', catchAsync(async (req, res) => {
+    try{
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.flash('success','Welcome to RecipeJar');
+        res.redirect('/recipes');
+    }catch(e){
+        req.flash('error',e.message);
+        res.redirect('/register');
+    }
+}));
 module.exports = router;
